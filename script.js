da603c8decd769367cee719701835ed5e9284d36
Jacob S <jacob.g.stewart@gmail.com>
2021-03-04 15:13:54 -0500

    optimizing drop down

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const listItems = document.querySelector('ul');
 
 listItems.addEventListener('click', e => {
-  if (e.target.tagName === 'H4') {
-    const targetParent = e.target.parentElement;
-    targetParent.classList.toggle('active');
+  if (e.target.tagName === 'H4' ||
+      e.target.tagName === 'SPAN' ||
+      e.target.tagName === 'IMG') {
+        const targetParent = e.target.closest('li');
+        targetParent.classList.toggle('active');
   }
 });
